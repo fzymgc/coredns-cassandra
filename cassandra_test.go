09c13ea7d6c249ed82dfcd3733cda4e20a4f025b
Sean Brandt <sean@fzymgc.dev>
2020-01-27 21:37:12 -0500

    major rework, align more with redis plugin

--- cassandra_test.go
+++ cassandra_test.go
@@ -1,17 +1,53 @@
+// +build integration
+
 package cassandra
 
 import (
 	"os"
 	"testing"
+	"time"
 )
 
 var (
-	cassandraTest *CassandraDatastore
+	cassandraTest *Cassandra
 	zone          = "test.zone."
-	hosts         = []string{"127.0.0.1"}
+	hosts         = []string{"10.1.0.251","10.1.0.252","10.1.0.253"}
+	username = "coredns"
+	password = "a8ErHwGfpQ4hCQwyQTcuqyC4s"
+	keyspace = "coredns"
 )
 
 func TestMain(m *testing.M) {
+	// disable test
+
+	startTime := time.Now()
+	cassandraTest = New()
+	cassandraTest.contactPoints = hosts
+	cassandraTest.username = username
+	cassandraTest.password = password
+	cassandraTest.keyspace = keyspace
+	cassandraTest.Connect()
+	cassandraTest.LoadZones()
+
+	if cassandraTest.LastZoneUpdate.Before(startTime) {
+		panic("Invalid LastZoneUpdate")
+	}
+
+	for _, z := range cassandraTest.Zones {
+		println(z)
+	}
+
+	r, _, _ := cassandraTest.SOA("ftest.zone.","ftest.zone.")
+	for _, rr := range r {
+		println(rr.String())
+	}
+
+	r, _, _ = cassandraTest.A("test.zone.","foo.test.zone.")
+	for _, rr := range r {
+		println(rr.String())
+	}
+
+
 	//cluster := gocql.NewCluster("localhost")
 	//cluster.Keyspace = "zones"
 	//session, err := cluster.CreateSession()
@@ -34,5 +70,5 @@ func TestMain(m *testing.M) {
 }
 
 func TestCassandra_ServeDNS(t *testing.T) {
-
+	t.Skip("skipping integration test")
 }
---
+++ handler.go
@@ -0,0 +1,104 @@
+package cassandra
+
+import (
+	"context"
+	"github.com/coredns/coredns/plugin"
+	clog "github.com/coredns/coredns/plugin/pkg/log"
+	"github.com/coredns/coredns/request"
+	"github.com/miekg/dns"
+	"time"
+)
+
+// main entrypoint from coredns
+func (cass *Cassandra) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+	state := request.Request{W: w, Req: r}
+
+	qname := state.Name()
+	qtype := state.QType()
+
+	if time.Since(cass.LastZoneUpdate) > zoneUpdateTime {
+		cass.LoadZones()
+	}
+
+	zone := plugin.Zones(cass.Zones).Matches(qname)
+	clog.Debugf("attempting to serve zone: %s",zone)
+	if zone == "" {
+		return plugin.NextOrFailure(qname, cass.Next, ctx, w, r)
+	}
+
+	var answers []dns.RR
+	var extras []dns.RR
+	var err error
+
+	switch qtype {
+	case dns.TypeAXFR, dns.TypeIXFR:
+		return errorResponse(state, zone, dns.RcodeNotImplemented, err)
+
+	case dns.TypeA:
+		answers, extras, err = cass.A(zone,qname)
+		if err != nil {
+			panic(err)
+		}
+	case dns.TypeAAAA:
+		answers, extras, err = cass.AAAA(zone,qname)
+		if err != nil {
+			panic(err)
+		}
+	case dns.TypeCNAME:
+		answers, extras, err = cass.CNAME(zone,qname)
+		if err != nil {
+			panic(err)
+		}
+	case dns.TypeTXT:
+		answers, extras, err = cass.TXT(zone,qname)
+		if err != nil {
+			panic(err)
+		}
+	case dns.TypeNS:
+		answers, extras, err = cass.NS(zone,qname)
+		if err != nil {
+			panic(err)
+		}
+	case dns.TypeMX:
+		answers, extras, err = cass.MX(zone,qname)
+		if err != nil {
+			panic(err)
+		}
+	case dns.TypeSRV:
+		answers, extras, err = cass.SRV(zone,qname)
+		if err != nil {
+			panic(err)
+		}
+	case dns.TypeSOA:
+		answers, extras, err = cass.SOA(zone,qname)
+		if err != nil {
+			panic(err)
+		}
+	default:
+		return errorResponse(state, zone, dns.RcodeNotImplemented, err)
+	}
+
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Authoritative, m.RecursionAvailable, m.Compress = true, false, true
+
+	m.Answer = append(m.Answer, answers...)
+	m.Extra = append(m.Extra, extras...)
+
+	state.SizeAndDo(m)
+	m = state.Scrub(m)
+	_ = w.WriteMsg(m)
+	return dns.RcodeSuccess, nil
+}
+
+
+func errorResponse(state request.Request, zone string, rcode int, err error) (int, error) {
+	m := new(dns.Msg)
+	m.SetRcode(state.Req, rcode)
+	m.Authoritative, m.RecursionAvailable, m.Compress = true, false, true
+
+	state.SizeAndDo(m)
+	_ = state.W.WriteMsg(m)
+	// Return success as the rcode to signal we have written to the client.
+	return dns.RcodeSuccess, err
+}
